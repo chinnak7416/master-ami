fa554cb7876540523ae46711cea2fdb7e11a77ee
RAMBABU <chinnak7416@gmail.com>
2024-07-22 18:36:26 +0530

    Master AMi from Ec2-server  creation

--- main.tf
+++ main.tf
@@ -7,3 +7,24 @@ resource "aws_instance" "ami" {
     Name = "ami-server"
   }
 }
+
+resource "null_resource" "ansible" {
+  connection {
+    type     = "ssh"
+    user     = jsondecode(data.vault_generic_secret.ssh.data_json).ansible_user
+    password = jsondecode(data.vault_generic_secret.ssh.data_json).ansible_password
+    host     = aws_instance.ami.private_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo pip3.11 install ansible hvac"
+    ]
+  }
+}
+
+resource "aws_ami_from_instance" "ami" {
+  depends_on = [null_resource.ansible]
+  name               = "golden-ami-${formatdate("DD-MM-YY", timestamp())}"
+  source_instance_id = aws_instance.ami.id
+}
